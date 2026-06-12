a2e3c3f76504666e75ad7e485050c7ee24da1adc
ArryZ <pamudasankalpa22us@gmail.com>
2026-06-12 19:16:27 +0530

    chore: update site URL to www subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://arryz.dev",
+  site: "https://www.arryz.dev",
   compressHTML: true,
   trailingSlash: "ignore",
   integrations: [sitemap(), react()],
